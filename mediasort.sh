8db18d6991c0dd2db5ffe90913e34a516a45c62e
TheGingeraffe <hestonio@gmail.com>
2019-02-01 15:55:43 -0700

    Final first draft

--- mediasort.sh
+++ mediasort.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
-# Sets and moves to the directory to be operated on.
+# This script organizes images, videos, GIFs, and HTML files in the target directory.
+
+# Sets and confirms directory to be operated on.
 
 read -ep 'Directory to sort: ' POINTER;
 read -ep "Are you sure you would like to sort $POINTER? (y/n): " CONFIRMATION;
 
+# Tests for target directories in $POINTER and creates them if necessary.
+
 if [[ $CONFIRMATION = y ]]; then
-  cd $POINTER && \
-  echo "Moving to $POINTER"
+  if ! [[ -e $POINTER/{img,gif,vid,html} ]]; then
+    mkdir -p $POINTER/{img,gif,vid,html} && \
+    echo "Creating destination directories..."
+  else
+    echo "Sorting $POINTER..."
+  fi
 else
-  echo "Exiting.." && \
+  echo "Exiting..." && \
   exit 1
 fi
 
 # Changes whitespaces in filenames to underscores in preparation for the loop. 
 
-find $POINTER -maxdepth 1 -type f \
-  | while read f; do 
+find $POINTER -maxdepth 1 -type f -name '* *' \
+  | while read f; do
+  echo "Renaming $f"
   mv "$f" "${f// /_}"
-done
+  done
 
-# Loop that iterates on a list of files found above and moves them to the 
+# Loop that iterates on a list of files found in $POINTER and moves them to the 
 # appropriate directories
 
 for thing in $(find $POINTER -maxdepth 1 -type f); do
-  nameoffile=$(echo $thing | cut -f2 -d"/")
-  if [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == *[NEP]G ]] && ! [[ -f $POINTER/img/$nameoffile ]]; then
+  if [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == *[NEP]G ]] && ! [[ -f $POINTER/img/$thing ]]; then
     mv $thing $POINTER/img && \
-    echo "$nameoffile is an image, moving to $POINTER/img"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == GIF ]] && ! [[ -f $POINTER/gif/$nameoffile ]]; then
+    echo "$thing is an image, moving to $POINTER/img"
+  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == GIF ]] && ! [[ -f $POINTER/gif/$thing ]]; then
     mv $thing $POINTER/gif && \
-    echo "$nameoffile is a gif, moving to $POINTER/gif"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == ISO ]] && ! [[ -f $POINTER/vid/$nameoffile ]]; then
-    mv $thing $POINTER/vid && \ 
-    echo "$nameoffile is a video, moving to $POINTER/vid"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == WebM ]] && ! [[ -f $POINTER/html/$nameoffile ]]; then
+    echo "$thing is a gif, moving to $POINTER/gif"
+  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == ISO ]] && ! [[ -f $POINTER/vid/$thing ]]; then
     mv $thing $POINTER/vid && \
-    echo "$nameoffile is a video, moving to $POINTER/vid"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == HTML ]] && ! [[ -f $POINTER/$nameoffile ]]; then
+    echo "$thing is a video, moving to $POINTER/vid"
+  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == WebM ]] && ! [[ -f $POINTER/html/$thing ]]; then
+    mv $thing $POINTER/vid && \
+    echo "$thing is a video, moving to $POINTER/vid"
+  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == HTML ]] && ! [[ -f $POINTER/$thing ]]; then
     mv $thing $POINTER/html && \
-    echo "$nameoffile is an html file, moving to $POINTER/html"
+    echo "$thing is an html file, moving to $POINTER/html"
   else
-    echo "There is either a file with the same name as $nameoffile in the destination, or I'm not sure of $nameoffile's file type"
+    echo "There is either a file with the same name as $thing in the destination, or I'm not sure of $thing's file type"
   fi
 done
