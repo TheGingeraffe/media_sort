c9f7f9036ec14f62e62c7da22bcf9de3de19189a
TheGingeraffe <hestonio@gmail.com>
2019-12-26 18:02:19 -0700

    Made better

--- mediasort.sh
+++ mediasort.sh
@@ -3,51 +3,56 @@
 
 # Sets and confirms directory to be operated on.
 
-read -ep 'Directory to sort: ' POINTER;
-read -ep "Are you sure you would like to sort $POINTER? (y/n): " CONFIRMATION;
+read -ep 'Directory to sort: ' pointer;
+read -ep "Are you sure you would like to sort ${pointer}? (y/n): " confirmation;
 
-# Tests for target directories in $POINTER and creates them if necessary.
+# deletes trailing /
+# needs "" and space after colon
+if [[ "${pointer: -1}" == "/" ]]; then
+pointer="${pointer::-1}"
+fi
 
-if [[ $CONFIRMATION = y ]]; then
-  if ! [[ -e $POINTER/{img,gif,vid,html} ]]; then
-    mkdir -p $POINTER/{img,gif,vid,html} && \
-    echo "Creating destination directories..."
-  else
-    echo "Sorting $POINTER..."
-  fi
+if [[ "${confirmation}" = y ]]; then
+  echo "Sorting "${pointer}/sort"..."
 else
-  echo "Exiting..." && \
   exit 1
 fi
 
+# Homogenize sort dir by moving files to top level
+
+find "${pointer}/sort" -mindepth 2 -type f -exec mv -t "${pointer}/sort" {} +
+
 # Changes whitespaces in filenames to underscores in preparation for the loop. 
 
-find $POINTER -maxdepth 1 -type f -name '* *' \
-  | while read f; do
-  echo "Renaming $f"
-  mv "$f" "${f// /_}"
+find "${pointer}/sort" -type f -iname "* *" \
+  | while read sort_file; do
+  echo "Renaming ${sort_file}"
+  mv "${sort_file}" "${sort_file// /_}"
   done
 
-# Loop that iterates on a list of files found in $POINTER and moves them to the 
+# Loop that iterates on a list of files found in $pointer and moves them to the 
 # appropriate directories
 
-for thing in $(find $POINTER -maxdepth 1 -type f); do
-  if [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == *[NEP]G ]] && ! [[ -f $POINTER/img/$thing ]]; then
-    mv $thing $POINTER/img && \
-    echo "$thing is an image, moving to $POINTER/img"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == GIF ]] && ! [[ -f $POINTER/gif/$thing ]]; then
-    mv $thing $POINTER/gif && \
-    echo "$thing is a gif, moving to $POINTER/gif"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == ISO ]] && ! [[ -f $POINTER/vid/$thing ]]; then
-    mv $thing $POINTER/vid && \
-    echo "$thing is a video, moving to $POINTER/vid"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == WebM ]] && ! [[ -f $POINTER/html/$thing ]]; then
-    mv $thing $POINTER/vid && \
-    echo "$thing is a video, moving to $POINTER/vid"
-  elif [[ $(file $thing | cut -f2 -d":" | cut -f2 -d" ") == HTML ]] && ! [[ -f $POINTER/$thing ]]; then
-    mv $thing $POINTER/html && \
-    echo "$thing is an html file, moving to $POINTER/html"
-  else
-    echo "There is either a file with the same name as $thing in the destination, or I'm not sure of $thing's file type"
+md_sort () {
+pointer="${1}"
+destination="${2}"
+file_type="${3}"
+
+if ! [[ -d ${pointer}/${destination} ]]; then
+mkdir -p ${pointer}/${destination}
+fi
+
+for thing in $(find ${pointer}/sort -maxdepth 1 -type f); do
+  if [[ $(file ${thing} | cut -f2 -d":" | cut -f2 -d" ") == ${file_type} ]] && \
+  ! [[ -f "${pointer}/${destination}/${thing}" ]]; then
+    mv "${thing}" "${pointer}/${destination}"
+    echo "${thing} has type ${destination}, moving to ${pointer}/${destination}"
   fi
 done
+}
+
+md_sort ${pointer} 'img' '*[NEP]G'
+md_sort ${pointer} 'gif' 'GIF'
+md_sort ${pointer} 'vid' 'ISO'
+md_sort ${pointer} 'vid' 'WebM'
+md_sort ${pointer} 'html' 'HTML'
